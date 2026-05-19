7483098428de3225f4b854acfcb6e3f038fe37e3
Aaron Lewis <arcticgenes@gmail.com>
2026-05-18 19:04:30 -0700

    Update utorrent_amc.ps1

--- utorrent_amc.ps1
+++ utorrent_amc.ps1
@@ -1,33 +1,20 @@
 param(
-	[string]$file = "", 
-	[string]$dir = "",
+	[string]$dir = "", 
 	[string]$title = "",
-	[string]$state = "",
-	[string]$kind = ""
+	[string]$label = ""
 	)
   
 <#
  
   How to call from utorrent
 	called from Completed
-	  C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy unrestricted -file "E:\Scripts\utorrent_amc.ps1" -file "%F" -dir "%D" -title "%N" -state "%S" -kind "%K"
+	  C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy unrestricted -file "E:\Scripts\qbtorrent_amc.ps1" -title "%N" -dir "%F" -label "%L""
  
 #>
 
 $Filebot = "C:\Program Files\Filebot\filebot.exe"
 $plexIP = "10.10.0.40"
-$nasFolder = "\\arcticnas\NAS_Storage"
+$nasFolder = "\\vault10g\NAS_Storage"
 
-if ($kind -match "multi"){
-	if (-not (gci -path $dir -Include *.rar -Recurse)){ 
-        $action = "copy"
-    }
-    else {
-        $action = "move"
-    }
-}
-if ($kind -match "single"){
-	$action = "copy"
-}
 
-& $filebot -script fn:amc --output $nasFolder --log-file $nasFolder\amc.log --action $action --conflict override -non-strict --def plex=$plexIP "movieFormat=Movies/{n} - {y} ({vf})" unsorted=y "ut_state=$state" "ut_title=$title" "ut_kind=$kind" "ut_file=$file" "ut_dir=$dir"
+& $filebot -script fn:amc --output $nasFolder --log-file $nasFolder\amc.log --action duplicate --conflict auto -non-strict --def plex=$plexIP movieFormat="Movies/{n} - {y} ({vf})" seriesFormat="TV Shows/{plex.tail}" animeFormat="Anime/{plex.tail}" unsorted=y "ut_title=$title" "ut_dir=$dir" "ut_kind=multi" "ut_label=$label"

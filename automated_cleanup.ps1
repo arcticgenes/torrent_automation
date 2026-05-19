84c7d894a9a0dce7e9d9fbd39c6b7c619f021f3c
Aaron Lewis <arcticgenes@gmail.com>
2026-05-18 19:05:34 -0700

    Update automated_cleanup.ps1

--- automated_cleanup.ps1
+++ automated_cleanup.ps1
@@ -1,19 +1,6 @@
- param (
-[String]$server,
-[String]$port,
-[String]$user,
-[String]$pass
-)
+# User CLI found at https://github.com/fedarovich/qbittorrent-cli/wiki/Getting-Started
 
-[String]$Script:UtorrentUrl = "http://$server`:$port/gui/"
-[String]$Script:token = ""
-$Script:webClient = $null
-
-$baseDate = [datetime]'1/1/1970'
 $currentTime = Get-Date
-$baseFolder = "E:\Torrents\Finished"
-
-
 
 function Use-RunAs {    
     # Check if script is running as Adminstrator and if not use RunAs 
@@ -45,82 +32,38 @@ function Use-RunAs {
     }  
 } 
 
-function Utorrent-HttpGet {
-    param (
-    [string]$Command
-    )
-
-    if ([string]::IsNullOrEmpty($Script:token) -eq $true -or $Script:webClient -eq $null) {
-        $Script:webClient = new-object System.Net.WebClient
-        $Script:webClient.Headers.Add("user-agent", "PowerShell Script")
-     
-        if ([string]::IsNullOrEmpty($User) -eq $false) {
-            $Script:webClient.Credentials = new-object System.Net.NetworkCredential($User, $Pass)
-        }
- 
-        $tokenResponse = $Script:webClient.DownloadString($Script:UtorrentUrl + "token.html")
-        [string]$cookies =  $Script:webClient.ResponseHeaders["Set-Cookie"]
- 
-        if ($tokenResponse  -match ".*<div[^>]*id=[`"`']token[`"`'][^>]*>([^<]*)</div>.*") {
-            $Script:token = $matches[1]
-            $Script:webClient.Headers.Add("Cookie", $cookies)
-        }
-    }
-    $url = "$($UtorrentUrl)?$($Command)&token=$($Script:token)"
-    Write-Host ("Calling url`t$url")
-    $response = $Script:webClient.DownloadString($url)
-    $json = ConvertFrom-JSON $response
-    if($json.build -ne $null) {
-        Write-Host ("Success $($json.build)")
-    }
-    return $json
-}
-
-function Remove-Torrents {
-    param (
-        $finishedTorrents
-    )
-    foreach($torrent in $finishedTorrents){
-        Utorrent-HttpGet "action=removedata&hash=$($torrent.Hash)"
-    }
-
-    sleep -s 10
-
-    foreach($torrent in $finishedTorrents){
-        if($torrent.File){
-            Remove-item -Path $torrent.File -Recurse -Force -Confirm:$false
-        }
-    }
-}
 
 Use-RunAs
 
 $torrents = @()
-$finishedTorrents = @()
 
-$json = Utorrent-HttpGet "list=1"
-$json.torrents | Foreach-object{
-    $object = "" | select Name,Hash,Ratio,Status,Added_On,Label,File
-    $object.Name = $_[2] 
-    $object.Hash = $_[0]
-    $object.Ratio = ($_[7]/1000)
-    $object.Status = $_[21]
-    $object.Added_On = ([TimeZone]::CurrentTimeZone.ToLocalTime($basedate.AddSeconds($_[23])))
-    $object.Label = $_[11]
-     $object.File = $_[26]
-    if ($_[26] -eq $baseFolder){
-        $object.File = ""
-    }   
+$json = @(qbt torrent list -F json | convertfrom-json)
+
+$json| Foreach-object{
+    $object = "" | select Name,Hash,Ratio,Status,CompletedDate,Tags
+    $object.Name = $_.name
+    $object.Hash = $_.hash
+    $object.Ratio = $_.ratio
+    $object.Status = $_.state
+    $object.CompletedDate = [TimeZone]::CurrentTimeZone.ToLocalTime((Get-Date "1970-01-01T00:00:00").AddSeconds($_.completion_on))
+    $object.Tags = $_.tags
+    
     $torrents += $object
 }
 
+$finishedTorrents = @()
+
 Foreach($torrent in $torrents){
-    if ($torrent.Status -match "Seeding"){
-        if ($torrent.Ratio -ge 1.5 -AND $torrent.Label -match "Immortal Seed") {$finishedTorrents += $torrent}
-        if ($torrent.Added_On -lt $currentTime.AddHours(-25) -AND $torrent.Label -match "Immortal Seed") {$finishedTorrents += $torrent}
-        if ($torrent.Added_On -lt $currentTime.AddHours(-337)) {$finishedTorrents += $torrent}
-        if ($torrent.Ratio -ge 2.0) {$finishedTorrents += $torrent}
+    if ($torrent.Status -match "stalledUp|uploading"){
+        if ($torrent.Ratio -ge 1.5 -AND $torrent.Tags -contains "Immortal Seed") {$finishedTorrents += $torrent}
+        elseif ($torrent.CompletedDate -lt $currentTime.AddHours(-25) -AND $torrent.Tags -contains "Immortal Seed") {$finishedTorrents += $torrent}
+        elseif ($torrent.CompletedDate -lt $currentTime.AddHours(-337)) {$finishedTorrents += $torrent}
+        elseif ($torrent.Ratio -ge 2.0) {$finishedTorrents += $torrent}
     }
 }
 
-Remove-Torrents $finishedTorrents 
+
+Foreach($torrent in $finishedTorrents){
+
+    qbt torrent delete -f $torrent.Hash
+}
